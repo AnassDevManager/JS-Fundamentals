b64e6a7a9f6f296c0af793ec9b12b88ab0e0502f
AnassDevManager <anasslaioij@gmail.com>
2025-07-13 15:24:27 +0100

    add 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,12 +1,8 @@
-const arg1 = process.argv[2];
-const arg2 = parseInt(process.argv[3]);
-arg1 = "My number ";
+const arg = process.argv[2];
+const num = parseInt(arg);
 
-if (arg2 === undefined) {
+if (isNaN(num)) {
     console.log("Not a number");
-} else if (arg2 !== parseInt(arg2)) {
-    console.log("Not a number");
-} else if (arg2 >== 0  ) {
-
-    console.log(`${arg1} is ${arg2}`);
+} else {
+    console.log("My number: is " + num);
 }
